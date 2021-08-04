f85eb8618c98b82639a6b118db6d247f16d18459
Steven <steven.686295@gmail.com>
2021-08-04 15:29:03 -0400

    Added dao classes except ManagerService (that's currently being worked on)

---
+++ VendingMachine/src/main/java/com/mthree/vendingmachine/dao/InventoryDao.java
@@ -0,0 +1,15 @@
+/*
+ * @author Steven Nguyen
+ * @email: steven.686295@gmail.com
+ * @date: 
+ */
+
+package com.mthree.vendingmachine.dao;
+
+/**
+ * 
+ * @author Steven
+ */
+public class InventoryDao {
+
+}
