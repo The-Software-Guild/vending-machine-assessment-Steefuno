988c34d14a3aa0835e7132cc71ede87dae61d02d
Steven <steven.686295@gmail.com>
2021-08-05 10:40:00 -0400

    completed InventoryDao interface

--- VendingMachine/src/main/java/com/mthree/vendingmachine/dao/InventoryDao.java
+++ VendingMachine/src/main/java/com/mthree/vendingmachine/dao/InventoryDao.java
@@ -6,10 +6,34 @@
 
 package com.mthree.vendingmachine.dao;
 
+import com.mthree.vendingmachine.dto.Item;
+
 /**
  * 
  * @author Steven
  */
-public class InventoryDao {
-
+public interface InventoryDao {
+    /**
+     * Removes one of an item from the inventory
+     * @param itemName the name of the item to remove
+     */
+    public void removeItem(String itemName);
+    
+    /**
+     * Gets a String[] of all item names
+     * @return the String[]
+     */
+    public String[] getAllItems();
+    
+    /**
+     * Gets an item given the name
+     * @param itemName the item name
+     * @return the item
+     */
+    public Item getItem(String itemName);
+    
+    /**
+     * Closes the inventory saving any changes if necessary
+     */
+    public void close();
 }
